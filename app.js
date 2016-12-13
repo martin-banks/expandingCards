5a3c4c649f34a236b4d93d690ab50a902bed5cb8
Banks, Martin <anotherbanksy@gmail.com>
2016-12-13 19:23:29 +1100

    refactor expanding text styles. add kicker text field

--- app.js
+++ app.js
@@ -1,101 +1,83 @@
-"use strict"
-const util = require('./utility')
+(()=>{
+	"use strict"
 
-let content = {
-	title: 'This is the main title',
-	intro: 'Main Intro',
-	showmore: 'Show more',
-	showless: 'Show less',
-	articles: {
-		a: {
-			title: 'Card title',
-			intro: 'card intro',
-			text: 'card text',
-			image: 'testing.png',
-		},
-		b: {
-			title: 'Card title',
-			intro: 'card intro',
-			text: 'card text',
-			image: 'testing.png',
-		},
-		c: {
-			title: 'Card title',
-			intro: 'card intro',
-			text: 'card text',
-			image: 'testing.png',
-		},
-		d: {
-			title: 'Card title',
-			intro: 'card intro',
-			text: 'card text',
-			image: 'testing.png',
-		}
-	}
-}
+	const util = require('./utility')
+	let content = require('./content')
 
+	let cardTitle = (text)=> `<h2>${text}</h2>`
+	let cardIntro = (text)=> `<h4>${text}</h4>`
+	let cardKicker = (text)=> `<h6 class="cc_kicker">${text}</h6>`
+	let cardText = (text)=> `<p class='cc-cardtext'>${text}</p>`
+	let button = (icon)=> `<div class="cc-button">${icon}</div>`
 
-let cardTitle = (text)=> `<h2>${text}</h2>`
-let cardIntro = (text)=> `<h4>${text}</h4>`
-let cardText = (text)=> `<p class='cc-cardtext'>${text}</p>`
-let button = (icon)=> `<div class="cc-button">${icon}</div>`
+	let container = props=> `
+		<div 
+			class="cc-profileCard" 
+			style="background-image:url(../images/${props.image})"
+			data-key="${props.key}"
+			data-state='closed'
+		>
+			${props.content}
+		</div>
+		`
 
-let container = props=> `
-	<div 
-		class="cc-profileCard" 
-		style="background-image:url(../images/${props.image})"
-		data-key="${props.key}"
-		data-state='closed'
-	>
-		${props.content}
-	</div>
-	`
-
-
-let cardTemplate = (props)=>{
-	return container({
-		image: props.image,
-		key: props.key,
-		content: [
-			cardTitle(props.title),
-			cardIntro(props.intro),
-			cardText(''),
-			button(content.showmore)
-		].join('')
-	})
-}
-
-let composeAllCards = ()=>{
-	let keys = Object.keys(content.articles)
-	return keys.map(key=>{
-		return cardTemplate({
-			title: content.articles[key].title,
-			image: content.articles[key].image,
-			intro: content.articles[key].intro,
-			key: key
+	let cardTemplate = (props)=>{
+		return container({
+			image: props.image,
+			key: props.key,
+			content: [
+				props.kicker.length>0 ? cardKicker(props.kicker) : '',
+				props.title.length>0 ? cardTitle(props.title) : '',
+				props.intro.length>0 ? cardIntro(props.intro) : '',
+				cardText(''),
+				button(content.cta.showmore)
+			].join('')
 		})
-	}).join('')
-}
-
-
-document.querySelector('.appContainer').innerHTML = composeAllCards()
-
-util.delegate('.appContainer', 'click', '.cc-profileCard', (e)=>{
-	let thisCard = util.closest(e.target, '.cc-profileCard')
-	let thisKey = thisCard.getAttribute('data-key')
-	
-	if(thisCard.getAttribute('data-state') === 'closed'){
-		thisCard.querySelector('.cc-button').innerHTML = content.showless
-		thisCard.setAttribute('data-state', 'open')
-		thisCard.querySelector('.cc-cardtext').innerHTML = content.articles[thisKey].text
-		thisCard.querySelector('.cc-cardtext').classList.add('heightauto')
-		return
-	} else if(thisCard.getAttribute('data-state') === 'open'){
-		thisCard.querySelector('.cc-button').innerHTML = content.showmore
-		thisCard.setAttribute('data-state', 'closed')
-		thisCard.querySelector('.cc-cardtext').innerHTML = ''
-		thisCard.querySelector('.cc-cardtext').classList.remove('heightauto')
-		return
 	}
-})
 
+	let composeAllCards = ()=>{
+		let keys = Object.keys(content.articles)
+		return keys.map(key=>{
+			let article = content.articles[key]
+			return cardTemplate({
+				kicker: article.kicker,
+				title: article.title,
+				image: article.image,
+				intro: article.intro,
+				key: key
+			})
+		}).join('')
+	}
+
+
+	document.querySelector('.cc_expandingCard_appContainer').innerHTML = [
+		composeAllCards()
+	].join('')
+
+
+	util.delegate('.cc_expandingCard_appContainer', 'click', '.cc-profileCard', (e)=>{
+		let thisCard = util.closest(e.target, '.cc-profileCard')
+		let thisKey = thisCard.getAttribute('data-key')
+		
+		if(thisCard.getAttribute('data-state') === 'closed'){
+			thisCard.querySelector('.cc-button').innerHTML = content.cta.showless
+			thisCard.setAttribute('data-state', 'open')
+			thisCard.querySelector('.cc-cardtext').innerHTML = content.articles[thisKey].text
+			thisCard.querySelector('.cc-cardtext').style.opacity = 1
+			thisCard.querySelector('.cc-cardtext').style.height = '100px'
+			return
+		
+		} else if(thisCard.getAttribute('data-state') === 'open'){
+			thisCard.querySelector('.cc-button').innerHTML = content.cta.showmore
+			thisCard.setAttribute('data-state', 'closed')
+			thisCard.querySelector('.cc-cardtext').innerHTML = ''
+			thisCard.querySelector('.cc-cardtext').style.opacity = ''
+			thisCard.querySelector('.cc-cardtext').style.height = ''
+			return
+		}
+	})
+
+
+
+
+})()
---
+++ content.js
@@ -0,0 +1,43 @@
+module.exports = {
+	headerBlock: {
+		kicker:'', 
+		title: 'This is the main title',
+		intro: 'Main Intro',
+	},
+
+	articles: {
+		a: {
+			kicker: '',
+			title: 'Card title',
+			intro: 'card intro',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ex pariatur, ea cupiditate porro praesentium sequi. Ad sequi labore, reprehenderit ipsum eius nam deleniti similique explicabo aperiam sapiente, atque cum!',
+			image: 'testing.png',
+		},
+		b: {
+			kicker: '',
+			title: 'Card title',
+			intro: 'card intro',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere eius harum, corrupti quasi labore iusto veniam ipsam nam nostrum ad quas optio sed molestiae dicta, doloremque molestias deleniti aliquam vero.',
+			image: 'testing.png',
+		},
+		c: {
+			kicker: '',
+			title: 'Card title',
+			intro: 'card intro',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni deserunt, fugit iste quos voluptatem laudantium voluptatibus mollitia earum vitae, voluptatum, modi optio deleniti laborum quis. Sed saepe non maiores, aut?',
+			image: 'testing.png',
+		},
+		d: {
+			kicker: '',
+			title: 'Card title',
+			intro: 'card intro',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis laborum incidunt fugit error dolores perferendis, nemo amet temporibus sequi eius optio eum molestiae, aliquid magnam odio laboriosam consectetur, vitae et.',
+			image: 'testing.png',
+		}
+	},
+
+	cta: {
+		showmore: 'Show more',
+		showless: 'Show less',
+	}
+}
