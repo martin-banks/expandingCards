9a2e8175d707f86786889de38e2aed6500a96657
Martin Banks <anotherbanksy@gmail.com>
2016-12-14 21:50:25 +1100

    update card content structure options:

--- content.js
+++ content.js
@@ -1,43 +1,54 @@
 module.exports = {
-	headerBlock: {
-		kicker:'', 
-		title: 'This is the main title',
-		intro: 'Main Intro',
-	},
-
-	articles: {
-		a: {
-			kicker: '',
-			title: 'Card title',
-			intro: 'card intro',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos ex pariatur, ea cupiditate porro praesentium sequi. Ad sequi labore, reprehenderit ipsum eius nam deleniti similique explicabo aperiam sapiente, atque cum!',
-			image: 'testing.png',
-		},
-		b: {
-			kicker: '',
-			title: 'Card title',
-			intro: 'card intro',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere eius harum, corrupti quasi labore iusto veniam ipsam nam nostrum ad quas optio sed molestiae dicta, doloremque molestias deleniti aliquam vero.',
-			image: 'testing.png',
-		},
-		c: {
-			kicker: '',
-			title: 'Card title',
-			intro: 'card intro',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni deserunt, fugit iste quos voluptatem laudantium voluptatibus mollitia earum vitae, voluptatum, modi optio deleniti laborum quis. Sed saepe non maiores, aut?',
-			image: 'testing.png',
-		},
-		d: {
-			kicker: '',
-			title: 'Card title',
-			intro: 'card intro',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis laborum incidunt fugit error dolores perferendis, nemo amet temporibus sequi eius optio eum molestiae, aliquid magnam odio laboriosam consectetur, vitae et.',
-			image: 'testing.png',
-		}
-	},
-
 	cta: {
 		showmore: 'Show more',
 		showless: 'Show less',
+	},
+
+	titleBlock: {
+		kicker:'', 
+		title: 'This is the main title',
+		intro: 'Main Intro',
+		image: ''
+	},
+
+	cards: {
+		a: {
+			intro: {
+				kicker: '',
+				title: 'Card title',
+				intro: 'card intro',	
+			},
+			displayImage: {
+				image: '',
+			}
+			body: [
+				'Some paragraph of text',
+				{
+					type: 'image', 
+					name: 'image.jpg',
+					caption: 'Image caption',
+					credit: 'image credit'					
+				},
+				{
+					type: 'list',
+					entries: [
+						'First list item',
+						'Second list item'
+					]
+				},
+				{
+					type: 'crosshead',
+					text: 'Crosshead text'
+				},
+				{
+					type: 'linkButton',
+					text: '',
+					link: ''
+				}
+			] 
+		}/*end of card*/
+
 	}
+
+	
 }
